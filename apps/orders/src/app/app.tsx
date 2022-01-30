f73a0ed4a69ae4e7dc253291bf0c0f309e9ca089
Domonkos Papp <pappdomonkos96@gmail.com>
2022-01-30 08:27:13 +0100

    Show orders

--- apps/orders/src/app/app.tsx
+++ apps/orders/src/app/app.tsx
@@ -1,9 +1,43 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  Grid,
+} from '@mui/material';
+import { getAllOrders } from '../fake-api';
+import './app.module.scss';
 
 export function App() {
   return (
-    <p>Hello</p>
+    <Grid container spacing={2}>
+      {getAllOrders().map((o) => (
+        <Grid item xs={8}>
+          <Card key={o.id} className="orderCard">
+            <CardContent>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                {o.ticker}
+              </Typography>
+              <Typography variant="h5" component="div">
+                {o.amount}
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                {o.date}
+              </Typography>
+              <Typography variant="body2">{o.price}</Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Open Details</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
   );
 }
 
