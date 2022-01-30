d59804ac2083526ca7ef63194c6a5cebcba01d7c
Domonkos Papp <pappdomonkos96@gmail.com>
2022-01-30 07:05:24 +0100

    Create fake api

---
+++ apps/orders/src/fake-api/index.ts
@@ -0,0 +1,37 @@
+const orders = [
+  {
+    id:1,
+    ticker: "TSLA",
+    amount:5,
+    price:835.2,
+    fee: 13,
+    currency:"USD",
+    marketPlace: "Frankfurt",
+    date:"2022.01.29",
+    created:"2022.01.29",
+  },
+  {
+    id:2,
+    ticker: "PLTR",
+    amount:85,
+    price:11.25,
+    fee: 13,
+    currency:"USD",
+    marketPlace: "Frankfurt",
+    date:"2022.01.25",
+    created:"2022.01.25",
+  },
+  {
+    id:3,
+    ticker: "DBX",
+    amount:30,
+    price:22.0,
+    fee: 13,
+    currency:"USD",
+    marketPlace: "NASDAQ",
+    date:"2022.01.22",
+    created:"2022.01.22",
+  }
+]
+export const getAllOrders = () => orders;
+export const getOrder = (id:number) => orders.find(order => order.id === id);
