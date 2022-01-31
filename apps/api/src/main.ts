783a5c9e958f742511b1c950df5ba6efdb1d4155
Domonkos Papp <pappdomonkos96@gmail.com>
2022-01-31 17:35:52 +0100

    Implement orders api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,11 +4,16 @@
  */
 
 import * as express from 'express';
+import { getAllOrders, getOrder } from './app/order.repository';
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
+app.get('/api/orders', (req, res) => {
+  res.send(getAllOrders());
+});
+
+app.get('/api/orders/:id', (req, res) => {
+  res.send(getOrder(parseInt(req.params.id)));
 });
 
 const port = process.env.port || 3333;
